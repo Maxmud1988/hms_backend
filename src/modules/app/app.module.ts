495d3ec286f121b4d96e81e4ce2b14e6a9e9b254
Makhmud <M.Xudoyberdiev88@gmail.com>
2023-04-12 10:56:49 +0300

    Create Watchlist

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -5,9 +5,14 @@ import { UserModule } from 'src/modules/user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config/dist';
 import configuretions from '../../configurations'
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from '../user/modules/user.model';
+import { User } from '../user/models/user.model';
 import { AuthModule } from '../auth/auth.module';
 import { TokenModule } from '../token/token.module';
+import { WatchlistModule } from '../watchlist/watchlist.module';
+import { Watchlist } from '../watchlist/models/watchlist.model';
+
+
+
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -26,14 +31,15 @@ import { TokenModule } from '../token/token.module';
       database: configService.get('db_name'),
       synchronize: true,
       autoLoadModels: true,
-      models: [User]
+      models: [User, Watchlist]
 
     })
 
   }),
     UserModule,
     AuthModule,
-    TokenModule
+    TokenModule,
+    WatchlistModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from './modules/user.model';
+import { User } from './models/user.model';
+
 
 
 
